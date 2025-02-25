0cdb8714f48bc54d64bfc16bc54c75f583e2f3c9
wlewisjr <wlewisjr_86@pm.me>
2025-02-25 07:20:02 -0500

    initial boilerplate

---
+++ main.py
@@ -0,0 +1,10 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+if __name__ == "__main__":
+    main()
